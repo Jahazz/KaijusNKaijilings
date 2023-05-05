f352c511b3e3c2cf37a109bda77eaba3a9f80d65
jakub dyja <jakub.dyja@gmail.com>
2023-05-05 13:39:23 +0200

    pre moving functions as Ienumerable delegates

--- Assets/Battle/BattleCore/Battle.cs
+++ Assets/Battle/BattleCore/Battle.cs
@@ -2,6 +2,11 @@ using System.Collections.Generic;
 using Utils;
 using System.Linq;
 using BattleCore.Actions;
+using BattleCore.ActionQueue;
+using System;
+using System.Collections;
+using BattleCore.ScreenEntity;
+using UnityEngine;
 
 namespace BattleCore
 {
@@ -13,9 +18,19 @@ namespace BattleCore
         public event OnEntitySkillUsageParams OnSkillUsage;
         public delegate void OnAllActionsSelectedParams ();
         public event OnAllActionsSelectedParams OnAllActionsSelected;
+        public delegate void OnBattleFinishedParams (BattleResultType battleResult);
+        public event OnBattleFinishedParams OnBattleFinished;
+        public delegate void OnPlayerEntitySwapRequestParams (Action<Entity> callback);
+        public event OnPlayerEntitySwapRequestParams OnPlayerEntitySwapRequest;
+
+        public Func<Entity, IEnumerator> ViewEntitySwapAction { get; set; }//shopuld be more generalised and not bound with view, but for now it will suffice? idk
+        public Func<Entity, IEnumerator> ViewPlayAnimationAsEntity { get; set; }
+        public Func<Entity, IEnumerator> ViewWaitForAnimationFinished { get; set; }
+        public Func<Player, Entity, IEnumerator> ViewSwapEntity { get; set; }
 
         public List<BattleParticipant> BattleParticipantsCollection { get; private set; } = new List<BattleParticipant>();
-        public ObservableVariable<BattleState> CurrentBattleState { get; set; } = new ObservableVariable<BattleState>(BattleState.NONE);
+        public ObservableVariable<BattleState> CurrentBattleState { get; private set; } = new ObservableVariable<BattleState>(BattleState.NONE);
+        private BattleActionResolver BattleActionResolver { get; set; }
 
         public Battle (List<Player> participantsCollection)
         {
@@ -29,20 +44,14 @@ namespace BattleCore
 
                 participant.SelectFirstAliveEntity();
             }
+            OnAllActionsSelected += HandleOnAllActionsSelected;
+            SetBattleState(BattleState.ACTION_CHOOSE);
         }
 
         public void ExecuteAttackAction (AttackBattleAction attackAction)
         {
             attackAction.Skill.UseSkill(attackAction.CasterOwner, attackAction.Caster, attackAction.Target.CurrentEntity.PresentValue, this);
             OnSkillUsage?.Invoke(attackAction);
-        }
-
-        public void ClearChosenBattleActions ()
-        {
-            foreach (BattleParticipant participant in BattleParticipantsCollection)
-            {
-                participant.SelectedBattleAction.PresentValue = null;
-            }
         }
 
         public BattleParticipant GetPlayerBattleParticipant ()
@@ -113,7 +122,204 @@ namespace BattleCore
 
         private void HandleCurrentEntityChanged (Entity AddedEntity, BattleParticipant entityOwner)
         {
-            AddedEntity.IsAlive.OnVariableChange += (_) => OnEntityDeath.Invoke(AddedEntity, entityOwner);
+            AddedEntity.IsAlive.OnVariableChange += (_) => HandleEntityDeath(AddedEntity, entityOwner);
+        }
+
+        private void HandleEntityDeath (Entity deadEntity, BattleParticipant entityOwner)
+        {
+            OnEntityDeath.Invoke(deadEntity, entityOwner);
+
+            if (BattleActionResolver != null)
+            {
+                BattleActionResolver.ActionQueueHandler.RemoveActionsWithPredicate(AttackActionRemovalPredicate);
+
+                bool AttackActionRemovalPredicate (QueuedAction target)
+                {
+                    bool output = false;
+
+                    if (target.ActionIsBasedOn.ActionType == BattleActionType.ATTACK)
+                    {
+                        AttackBattleAction attackAction = target.ActionIsBasedOn as AttackBattleAction;
+
+                        output = attackAction.Caster == deadEntity;
+                    }
+
+                    return output;
+                }
+
+            }
+        }
+
+        private void SetBattleState (BattleState newValue)
+        {
+            CurrentBattleState.PresentValue = newValue;
+
+            switch (newValue)
+            {
+                case BattleState.ACTION_CHOOSE:
+                    ActionChoose();
+                    break;
+                case BattleState.ACTION_RESOLVE:
+                    ActionResolve();
+                    break;
+                case BattleState.WRAP_UP:
+                    WrapUp();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void ActionChoose ()
+        {
+            // ENEMY ACTION FAKE CHOOSE
+            BattleParticipant enemy = GetNPCBattleParticipant();
+            enemy.QueueAttackAction(enemy.CurrentEntity.PresentValue, GetPlayerBattleParticipant(), enemy.CurrentEntity.PresentValue.SelectedSkillsCollection[0]);
+        }
+
+        private void ActionResolve ()
+        {
+            InitializeBattleResolver();
+            BattleActionResolver.SortActions(BattleParticipantsCollection);
+            BattleActionResolver.ResolveAllActions();
+        }
+
+        private void WrapUp ()
+        {
+            if (CheckIsBattleOver(out BattleResultType battleResult) == true)
+            {
+                OnBattleFinished?.Invoke(battleResult);
+            }
+            else if (GetPlayerBattleParticipant().CurrentEntity.PresentValue.IsAlive.PresentValue == false)
+            {
+                HandlePlayerEntitySwap();
+            }
+            else if (GetNPCBattleParticipant().CurrentEntity.PresentValue.IsAlive.PresentValue == false)
+            {
+                HandleEnemyEntitySwapIfNeeded();
+            }
+            else
+            {
+                FinishWrapUp();
+            }
+        }
+        private void InitializeBattleResolver ()
+        {
+            BattleActionResolver = new BattleActionResolver(HandleOnAllActionsResolved);
+
+            BattleActionResolver.OnAttackActionResolution += AddAttackActionToQueue;
+            BattleActionResolver.OnDisengageActionResolution += AddDisengageActionToQueue;
+            BattleActionResolver.OnItemActionResolution += AddItemActionToQueue;
+            BattleActionResolver.OnSwapActionResolution += AddSwapActionToQueue;
+        }
+
+        private void HandleOnAllActionsSelected ()
+        {
+            SetBattleState(BattleState.ACTION_RESOLVE);
+        }
+
+        private void HandleOnAllActionsResolved ()
+        {
+            DisposeOfBattleResolver();
+            SetBattleState(BattleState.WRAP_UP);
+        }
+
+        private void FinishWrapUp ()
+        {
+            ClearChosenBattleActions();
+            SetBattleState(BattleState.ACTION_CHOOSE);
+        }
+
+        private void ClearChosenBattleActions ()
+        {
+            foreach (BattleParticipant participant in BattleParticipantsCollection)
+            {
+                participant.SelectedBattleAction.PresentValue = null;
+            }
+        }
+
+        private void DisposeOfBattleResolver ()
+        {
+            BattleActionResolver.OnAttackActionResolution -= AddAttackActionToQueue;
+            BattleActionResolver.OnDisengageActionResolution -= AddDisengageActionToQueue;
+            BattleActionResolver.OnItemActionResolution -= AddItemActionToQueue;
+            BattleActionResolver.OnSwapActionResolution -= AddSwapActionToQueue;
+
+            BattleActionResolver = null;
+        }
+
+        private void HandleEntitySwap (BattleParticipant participant, Entity entityToSwap, Action finishCallback)
+        {
+            QueueHandler swapQueue = new QueueHandler(finishCallback);
+            AddSwapActionToQueue(new SwapBattleAction(participant, entityToSwap), swapQueue);
+            swapQueue.InvokeActions();
+        }
+
+        private void HandleEnemyEntitySwapIfNeeded ()
+        {
+            BattleParticipant npc = GetNPCBattleParticipant();
+
+            if (npc.CurrentEntity.PresentValue.IsAlive.PresentValue == false)
+            {
+                HandleEntitySwap(npc, npc.GetFirstAliveEntity(), FinishWrapUp);
+            }
+            else
+            {
+                FinishWrapUp();
+            }
+        }
+
+        private void HandlePlayerEntitySwap ()
+        {
+            BattleParticipant player = GetPlayerBattleParticipant();
+
+            OnPlayerEntitySwapRequest.Invoke(SwapPlayerEntity);
+
+            void SwapPlayerEntity (Entity entityToSwapTo)
+            {
+                HandleEntitySwap(player, entityToSwapTo, HandleEnemyEntitySwapIfNeeded);
+            }
+        }
+
+        private void AddSwapActionToQueue (BaseBattleAction battleAction, QueueHandler actionQueue)
+        {
+            SwapBattleAction swapAction = battleAction as SwapBattleAction;
+
+            actionQueue.EnqueueFunction(battleAction, () => ViewEntitySwapAction(swapAction.ActionOwner.CurrentEntity.PresentValue));
+            actionQueue.EnqueueFunction(battleAction, () => SwapEntityVariable(swapAction.ActionOwner, swapAction.EntityToSwapTo));
+            actionQueue.EnqueueFunction(battleAction, () => ViewSwapEntity(swapAction.ActionOwner.Player, swapAction.EntityToSwapTo));
+        }
+
+        private void AddAttackActionToQueue (BaseBattleAction battleAction, QueueHandler actionQueue)
+        {
+            AttackBattleAction attackAction = battleAction as AttackBattleAction;
+
+            actionQueue.EnqueueFunction(battleAction, () => ViewPlayAnimationAsEntity(attackAction.Caster));
+            actionQueue.EnqueueFunction(battleAction, () => ExecuteAttackActionDelegate(attackAction));
+            actionQueue.EnqueueFunction(battleAction, () => ViewWaitForAnimationFinished(attackAction.Caster));
+        }
+
+        private void AddDisengageActionToQueue (BaseBattleAction battleAction, QueueHandler actionQueue)
+        {
+            DisengageBattleAction disengageAction = battleAction as DisengageBattleAction;
+        }
+
+        private void AddItemActionToQueue (BaseBattleAction battleAction, QueueHandler actionQueue)
+        {
+            UseItemBattleAction useItemBattleAction = battleAction as UseItemBattleAction;
+        }
+
+
+        private IEnumerator SwapEntityVariable (BattleParticipant participant, Entity entityToSwapTo)
+        {
+            participant.CurrentEntity.PresentValue = entityToSwapTo;
+            yield return null;
+        }
+
+        private IEnumerator ExecuteAttackActionDelegate (AttackBattleAction selectedAction)
+        {
+            ExecuteAttackAction(selectedAction);
+            yield return null;
         }
     }
 }
--- Assets/Battle/UI/BattleScreen/BattleScreenModel.cs
+++ Assets/Battle/UI/BattleScreen/BattleScreenModel.cs
@@ -16,7 +16,6 @@ namespace BattleCore.UI
         private BattleScreenSummaryController BattleScreenSummaryController { get; set; }
         [field: SerializeField]
         private CharacterMenuController CharacterMenuController { get; set; }
-        private BattleActionResolver BattleActionResolver { get; set; }
         private Battle CurrentBattle { get; set; }
 
         public void QueuePlayerSkillUsage (Entity caster, SkillScriptableObject skill)
@@ -61,204 +60,40 @@ namespace BattleCore.UI
                 StartCoroutine(ChangePlayerEntity(battleParticipant.Player, battleParticipant.CurrentEntity.PresentValue));
             }
 
-            CurrentBattle.OnEntityDeath += HandleOnEntityDeath;
+            CurrentBattle.OnEntityDeath += CurrentBattle_OnEntityDeath;
             CurrentBattle.CurrentBattleState.OnVariableChange += HandleOnCurrentBattleStateChange;
-            CurrentBattle.OnAllActionsSelected += HandleOnAllActionsSelected;
+            CurrentBattle.OnBattleFinished += HandleOnBattleFinished;
+            CurrentBattle.OnPlayerEntitySwapRequest += HandleOnPlayerEntitySwapRequest;
 
-            CurrentBattle.CurrentBattleState.PresentValue = BattleState.ACTION_CHOOSE;
+            CurrentBattle.ViewEntitySwapAction = CurrentView.TryToDestroyEntityOnScene;
+            CurrentBattle.ViewPlayAnimationAsEntity =(entity) => CurrentView.PlayAnimationAsEntity(entity,AnimationType.ATTACK);
+            CurrentBattle.ViewWaitForAnimationFinished = CurrentView.WaitUntilAnimatorIdle;
+
 
             CurrentView.IsBottomBarShown(true);
         }
 
-        private void HandleOnAllActionsSelected ()
+        private void CurrentBattle_OnEntityDeath (Entity entity, BattleParticipant owner)
         {
-            CurrentBattle.CurrentBattleState.PresentValue = BattleState.ACTION_RESOLVE;
+            CurrentView.PlayAnimationAsEntity(entity, AnimationType.DIE);
+        }
+
+        private void HandleOnPlayerEntitySwapRequest (Action<Entity> callback)
+        {
+            CharacterMenuController.OpenMenuAsEntitySelection(callback);
+        }
+
+        private void HandleOnBattleFinished (BattleResultType battleResult)
+        {
+            BattleScreenSummaryController.OpenScreen(battleResult);
         }
 
         private void HandleOnCurrentBattleStateChange (BattleState newValue)
         {
             CurrentView.SetBottomUIBarInteractible(newValue == BattleState.ACTION_CHOOSE);
-
-            switch (newValue)
-            {
-                case BattleState.ACTION_CHOOSE:
-                    ActionChoose();
-                    break;
-                case BattleState.ACTION_RESOLVE:
-                    ActionResolve();
-                    break;
-                case BattleState.WRAP_UP:
-                    WrapUp();
-                    break;
-                default:
-                    break;
-            }
-        }
-
-        private void ActionChoose ()
-        {
-            // ENEMY ACTION FAKE CHOOSE
-            BattleParticipant enemy = CurrentBattle.GetNPCBattleParticipant();
-            enemy.QueueAttackAction(enemy.CurrentEntity.PresentValue, CurrentBattle.GetPlayerBattleParticipant(), enemy.CurrentEntity.PresentValue.SelectedSkillsCollection[0]);
-        }
-
-        private void ActionResolve ()
-        {
-            InitializeBattleResolver();
-            BattleActionResolver.SortActions(CurrentBattle.BattleParticipantsCollection);
-            BattleActionResolver.ResolveAllActions();
-        }
-
-        private void WrapUp ()
-        {
-            if (CurrentBattle.CheckIsBattleOver(out BattleResultType battleResult) == true)
-            {
-                BattleScreenSummaryController.OpenScreen(battleResult);
-            }
-            else if (CurrentBattle.GetPlayerBattleParticipant().CurrentEntity.PresentValue.IsAlive.PresentValue == false)
-            {
-                HandlePlayerEntitySwap();
-            }
-            else if (CurrentBattle.GetNPCBattleParticipant().CurrentEntity.PresentValue.IsAlive.PresentValue == false)
-            {
-                HandleEnemyEntitySwapIfNeeded();
-            }
-            else
-            {
-                FinishWrapUp();
-            }
-        }
-
-        private void HandleEntitySwap (BattleParticipant participant, Entity entityToSwap, Action finishCallback)
-        {
-            QueueHandler swapQueue = new QueueHandler(finishCallback);
-            AddSwapActionToQueue(new SwapBattleAction(participant, entityToSwap), swapQueue);
-            swapQueue.InvokeActions();
-        }
-
-        private void HandleEnemyEntitySwapIfNeeded ()
-        {
-            BattleParticipant npc = CurrentBattle.GetNPCBattleParticipant();
-
-            if (npc.CurrentEntity.PresentValue.IsAlive.PresentValue == false)
-            {
-                HandleEntitySwap(npc, npc.GetFirstAliveEntity(), FinishWrapUp);
-            }
-            else
-            {
-                FinishWrapUp();
-            }
-        }
-
-        private void HandlePlayerEntitySwap ()
-        {
-            BattleParticipant player = CurrentBattle.GetPlayerBattleParticipant();
-            CharacterMenuController.OpenMenuAsEntitySelection(SwapPlayerEntity);
-
-            void SwapPlayerEntity (Entity entityToSwapTo)
-            {
-                HandleEntitySwap(player, entityToSwapTo, HandleEnemyEntitySwapIfNeeded);
-            }
-        }
-
-        private void FinishWrapUp ()
-        {
-            CurrentBattle.ClearChosenBattleActions();
-            CurrentBattle.CurrentBattleState.PresentValue = BattleState.ACTION_CHOOSE;
         }
 
 
-
-        private void InitializeBattleResolver ()
-        {
-            BattleActionResolver = new BattleActionResolver(HandleOnAllActionsResolved);
-
-            BattleActionResolver.OnAttackActionResolution += AddAttackActionToQueue;
-            BattleActionResolver.OnDisengageActionResolution += AddDisengageActionToQueue;
-            BattleActionResolver.OnItemActionResolution += AddItemActionToQueue;
-            BattleActionResolver.OnSwapActionResolution += AddSwapActionToQueue;
-        }
-
-        private void HandleOnAllActionsResolved ()
-        {
-            DisposeOfBattleResolver();
-            CurrentBattle.CurrentBattleState.PresentValue = BattleState.WRAP_UP;
-        }
-
-        private void DisposeOfBattleResolver ()
-        {
-            BattleActionResolver.OnAttackActionResolution -= AddAttackActionToQueue;
-            BattleActionResolver.OnDisengageActionResolution -= AddDisengageActionToQueue;
-            BattleActionResolver.OnItemActionResolution -= AddItemActionToQueue;
-            BattleActionResolver.OnSwapActionResolution -= AddSwapActionToQueue;
-
-            BattleActionResolver = null;
-        }
-
-        private void AddSwapActionToQueue (BaseBattleAction battleAction, QueueHandler actionQueue)
-        {
-            SwapBattleAction swapAction = battleAction as SwapBattleAction;
-
-            actionQueue.EnqueueFunction(battleAction, () => CurrentView.TryToDestroyEntityOnScene(swapAction.ActionOwner.CurrentEntity.PresentValue));
-            actionQueue.EnqueueFunction(battleAction, () => SwapEntityVariable(swapAction.ActionOwner, swapAction.EntityToSwapTo));
-            actionQueue.EnqueueFunction(battleAction, () => ChangePlayerEntity(swapAction.ActionOwner.Player, swapAction.EntityToSwapTo));
-        }
-
-        private void AddAttackActionToQueue (BaseBattleAction battleAction, QueueHandler actionQueue)
-        {
-            AttackBattleAction attackAction = battleAction as AttackBattleAction;
-
-            actionQueue.EnqueueFunction(battleAction, () => CurrentView.PlayAnimationAsEntity(attackAction.Caster, AnimationType.ATTACK));
-            actionQueue.EnqueueFunction(battleAction, () => ExecuteAttackAction(attackAction));
-            actionQueue.EnqueueFunction(battleAction, () => CurrentView.WaitUntilAnimatorIdle(attackAction.Caster));
-        }
-
-        private void AddDisengageActionToQueue (BaseBattleAction battleAction, QueueHandler actionQueue)
-        {
-            DisengageBattleAction disengageAction = battleAction as DisengageBattleAction;
-        }
-
-        private void AddItemActionToQueue (BaseBattleAction battleAction, QueueHandler actionQueue)
-        {
-            UseItemBattleAction useItemBattleAction = battleAction as UseItemBattleAction;
-        }
-
-        private IEnumerator SwapEntityVariable (BattleParticipant participant, Entity entityToSwapTo)
-        {
-            participant.CurrentEntity.PresentValue = entityToSwapTo;
-            yield return null;
-        }
-
-        private IEnumerator ExecuteAttackAction (AttackBattleAction selectedAction)
-        {
-            CurrentBattle.ExecuteAttackAction(selectedAction);
-            yield return null;
-        }
-
-        private void HandleOnEntityDeath (Entity entity, BattleParticipant entityOwner)
-        {
-            CurrentView.PlayAnimationAsEntity(entity, AnimationType.DIE);
-
-            if (BattleActionResolver != null)
-            {
-                BattleActionResolver.ActionQueueHandler.RemoveActionsWithPredicate(AttackActionRemovalPredicate);
-
-                bool AttackActionRemovalPredicate (QueuedAction target)
-                {
-                    bool output = false;
-
-                    if (target.ActionIsBasedOn.ActionType == BattleActionType.ATTACK)
-                    {
-                        AttackBattleAction attackAction = target.ActionIsBasedOn as AttackBattleAction;
-
-                        output = attackAction.Caster == entity;
-                    }
-
-                    return output;
-                }
-
-            }
-        }
 
         private IEnumerator ChangePlayerEntity (Player owner, Entity newValue)
         {
@@ -275,6 +110,12 @@ namespace BattleCore.UI
             }
 
             return output;
+        }
+
+        private IEnumerator ExecuteAttackAction (AttackBattleAction selectedAction)
+        {
+            CurrentBattle.ExecuteAttackAction(selectedAction);
+            yield return null;
         }
 
         private void SetupSkills (Entity entity)//TODO: add item skill
